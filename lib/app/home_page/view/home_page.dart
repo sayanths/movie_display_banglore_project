32727c655ba00f2fe90c46deb379d0143cea7ccc
sayanth A <sayanthsayu619@gmail.com>
2022-10-30 18:02:36 +0530

    ui api fetched

--- lib/app/home_page/view/home_page.dart
+++ lib/app/home_page/view/home_page.dart
@@ -1,6 +1,6 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:movie_project_banglore/app/core/fonts.dart';
+import 'package:movie_project_banglore/app/home_page/view/widgets/custom_home_container.dart';
 import 'package:movie_project_banglore/app/home_page/view_model/home_page.dart';
 
 import 'package:provider/provider.dart';
@@ -10,140 +10,140 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     context.read<HomeProvider>().fetchDatas();
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       appBar: AppBar(
-        foregroundColor: Colors.white,
-        backgroundColor: Theme.of(context).backgroundColor,
-        title: InkWell(
-          onTap: (){
-           // context.read<UserProvider>().accountInfo(context);
-          },
-          child: Text("null")),
-        actions: [
-          ElevatedButton(onPressed: (){
-            context.read<HomeProvider>().companyinfo(context);
-          }, child: const Text("company info"))
-        ]
-      ),
+          foregroundColor: Colors.white,
+          backgroundColor: Theme.of(context).backgroundColor,
+          title: InkWell(
+              onTap: () {
+                // context.read<UserProvider>().accountInfo(context);
+              },
+              child: const Text("")),
+          actions: [
+            ElevatedButton(
+                onPressed: () {
+                  context.read<HomeProvider>().companyinfo(context);
+                },
+                child: const Text("company info"))
+          ]),
       body: Consumer<HomeProvider>(
         builder: (context, value, child) {
           if (value.movieData == null) {
             return const Center(child: CircularProgressIndicator());
           } else {
-            return ListView.builder(
-                itemCount: value.movieData?.length,
-                itemBuilder: (context, index) {
-                  return SizedBox(
-                    width: 30,
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(0.0),
-                      ),
-                      color: Colors.white,
-                      elevation: 10,
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                                Expanded(
-                                  flex: 3,
-                                  child: ConstrainedBox(
-                                    constraints: const BoxConstraints(),
-                                    child: Image.network(
-                                        value.movieData![index].poster,
-                                        fit: BoxFit.fill),
-                                  ),
-                                ),
-                                Expanded(
-                                  flex: 6,
-                                  child: Padding(
-                                    padding: EdgeInsets.all(16.0),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: <Widget>[
-                                        Text(
-                                          value.movieData![index].title,
-                                          style: Theme.of(context)
-                                              .textTheme
-                                              .headlineMedium,
-                                        ),
-                                        SizedBox(
-                                          height: 10,
-                                        ),
-                                        Text(
-                                            "genere :  ${value.movieData?[index].genre ?? ""}"),
-                                        SizedBox(
-                                          height: 5,
-                                        ),
-                                        Text(
-                                            "language :  ${value.movieData?[index].language ?? ""}"
-                                              ..split('.')),
-                                        SizedBox(
-                                          height: 5,
-                                        ),
-                                        SizedBox(
-                                          height: 5,
-                                        ),
-                                        Text(
-                                          "Stars :  ${value.movieData?[index].stars.join().toString() ?? ""}",
-                                          maxLines: 2,
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                                Expanded(
-                                    flex: 2,
-                                    child: SizedBox(
-                                      child: Column(
-                                        children: [
-                                          const Icon(
-                                              Icons.arrow_drop_up_outlined,
-                                              size: 60),
-                                          Text(value
-                                                  .movieData?[index].totalVoted
-                                                  .toString() ??
-                                              "0"),
-                                          const Icon(
-                                            Icons.arrow_drop_down_outlined,
-                                            size: 60,
-                                          )
-                                        ],
-                                      ),
-                                    ))
-                              ],
-                            ),
-                            Padding(
-                              padding: EdgeInsets.all(20),
-                              child: Wrap(
-                                  alignment: WrapAlignment.spaceEvenly,
-                                  children: [
-                                    Text(
-                                        "views : ${value.movieData?[index].pageViews.toString() ?? ""}"),
-                                    SizedBox(
-                                      width: 30,
-                                    ),
-                                    Text(
-                                        value.movieData?[index].director
-                                                    .join()
-                                                    .isNotEmpty ==
-                                                true
-                                            ? "Director : ${value.movieData?[index].director.join().toString()}"
-                                            : "",
-                                        style: const TextStyle(
-                                            fontWeight: FontWeight.w600)),
-                                  ]),
-                            ),
-                          ],
-                        ),
-                      ),
+            return ListView.separated(
+                separatorBuilder: (context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 30),
+                    child: Divider(
+                      height: 10,
+                      thickness: 2,
                     ),
+                  );
+                },
+                itemCount: value.movieData!.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Expanded(
+                              flex: 2,
+                              child: SizedBox(
+                                child: Column(
+                                  children: [
+                                    const Icon(Icons.arrow_drop_up_outlined,
+                                        size: 50),
+                                    Text(value.movieData?[index].totalVoted
+                                            .toString() ??
+                                        "0"),
+                                    const Icon(
+                                      Icons.arrow_drop_down_outlined,
+                                      size: 60,
+                                    )
+                                  ],
+                                ),
+                              )),
+                          Expanded(
+                            flex: 3,
+                            child: Container(
+                              height: size.height / 6,
+                              width: size.width / 4,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                          value.movieData![index].poster))),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 6,
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    value.movieData![index].title.toString(),
+                                    style: gFontsSans(sz: 20),
+                                  ),
+                                  CustomRichText(
+                                      mainTitle: 'Genere :',
+                                      subTitle:
+                                          ' ${value.movieData?[index].genre ?? ""}'),
+                                  CustomRichText(
+                                      mainTitle: 'Language :',
+                                      subTitle:
+                                          '${value.movieData?[index].language ?? ""}'),
+                                  CustomRichText(
+                                      mainTitle: 'stars :',
+                                      subTitle: value.movieData?[index].stars
+                                              .join()
+                                              .toString() ??
+                                          ""),
+                                  Text(
+                                    "min | Enflis |2 Apr",
+                                    style:
+                                        gFontsSans(cl: Colors.blue, sz: 13),
+                                  ),
+                                  const Text("min | Enflis |2 Aprssdsd"),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Row(
+                           mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Text(
+                                  "views : ${value.movieData?[index].pageViews.toString() ?? ""}"),
+                              const SizedBox(
+                                width: 30,
+                              ),
+                              Text(
+                                  value.movieData?[index].director
+                                              .join()
+                                              .isNotEmpty ==
+                                          true
+                                      ? "Director : ${value.movieData?[index].director.join().toString()}"
+                                      : "",
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.w600)),
+                            ]),
+                      ),
+                      SizedBox(
+                          width: size.width / 1.1,
+                          child: ElevatedButton(
+                              onPressed: () {},
+                              child: const Text("Watch Trailer")))
+                    ],
                   );
                 });
           }
